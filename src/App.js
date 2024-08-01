d8c80836abbf82cc72cb9908d843124311c8c5b3
michaeldavidvinc1 <tawie18102000@gmail.com>
2024-08-01 18:45:05 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import LoginPage from './components/auth/LoginPage';
+import ProfilePage from './components/userpage/ProfilePage';
+import RegistrationPage from './components/auth/RegistrationPage';
+import UserManagementPage from './components/userpage/UserManagementPage';
+import UpdateUser from './components/userpage/UpdateUser';
+import Navbar from './components/common/Navbar';
+import FooterComponent from './components/common/Footer';
+import UserService from './components/service/UserService';
+
+
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route exact path="/" element={<LoginPage />} />
+            <Route exact path="/login" element={<LoginPage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+
+            {/* Check if user is authenticated and admin before rendering admin-only routes */}
+            {UserService.adminOnly() && (
+              <>
+                <Route path="/register" element={<RegistrationPage />} />
+                <Route path="/admin/user-management" element={<UserManagementPage />} />
+                <Route path="/update-user/:userId" element={<UpdateUser />} />
+              </>
+            )}
+            <Route path="*" element={<Navigate to="/login" />} />‰
+          </Routes>
+        </div>
+        <FooterComponent />
+      </div>
+    </BrowserRouter>
   );
 }
 
